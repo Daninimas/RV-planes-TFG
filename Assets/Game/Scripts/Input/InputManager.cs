d5ef02437599cfe3dfc8db95b06c63085305d96d
Daninimas <dbg29@alu.ua.es>
2021-03-17 12:59:56 +0100

    Started proyect and added plane joystick

---
+++ Assets/Game/Scripts/Input/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InputManager : MonoBehaviour
+{
+    private ActionBasedController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<ActionBasedController>();
+
+        //************** FORMAS DE OBTENER INPUT ***************
+
+        // Ver si un boton ha sido pulsado y esto devuelve true o false
+        bool isActionPressed = controller.selectAction.action.ReadValue<bool>();
+        // Cuando se pulsa un boton, se llama a una funcion
+        controller.selectAction.action.performed += PulsadoBotonSelect;
+    }
+
+    private void PulsadoBotonSelect(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Boton select pulsado");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Game/Scripts/Plane/PlaneJoystick.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneJoystick : MonoBehaviour
+{
+    public Transform joystickTransform;
+    public ConfigurableJoint joystickJoint;
+
+    private bool returnToTargetRotation = true;
+    public float returningRotationVelocity = 0f;
+
+    void Start()
+    {
+        if (joystickTransform == null)
+        {
+            joystickTransform = gameObject.transform;
+        }
+
+        if (joystickJoint == null)
+        {
+            joystickJoint = gameObject.GetComponent<ConfigurableJoint>();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (returnToTargetRotation)
+        {
+            // The step size is equal to speed times frame time.
+            var step = returningRotationVelocity * Time.deltaTime;
+
+            // Rotate our transform a step closer to the target's.
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, joystickJoint.targetRotation, step);
+        }
+    }
+
+    public void OnSelect()
+    {
+        // When the joystick picked
+        Debug.Log("On select event");
+        returnToTargetRotation = false;
+    }
+    public void OnDeselect()
+    {
+        // When the joystick is release
+        Debug.Log("On deselect event");
+        returnToTargetRotation = true;
+    }
+
+    public void OnActivate()
+    {
+        // When trigger pressed and it is been picked
+        Debug.Log("On activate event");
+    }
+    public void OnDeactivate()
+    {
+        // When trigger released and it is been picked
+        Debug.Log("On deactivate event");
+    }
+}
