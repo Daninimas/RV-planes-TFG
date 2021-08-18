b82d6084a17ea361a5cfc922f7ad4d09edaf7674
Daninimas <dbg29@alu.ua.es>
2021-08-18 14:41:39 +0200

    more terrain

---
+++ Assets/Game/Scripts/AI/Marker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marker : MonoBehaviour
+{
+    [Min(0)]
+    public float scale = 0.005f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform cameraTransform = Camera.main.transform;
+
+        // Look to camera
+        transform.LookAt(cameraTransform.position);
+
+        // Set the size depending from the distance to the camera
+        float sc = scale * Vector3.Distance(cameraTransform.position, this.transform.position);
+        this.transform.localScale = new Vector3(sc, sc, sc);
+    }
+}
